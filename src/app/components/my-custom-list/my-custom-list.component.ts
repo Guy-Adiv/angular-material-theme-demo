b247fba2ad2f7547954851600bd75c23088cb416
Guy Adiv <guya@avanan.com>
2020-09-30 10:42:06 +0300

    set custom component theme file

--- src/app/components/my-custom-list/my-custom-list.component.ts
+++ src/app/components/my-custom-list/my-custom-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-my-custom-list',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyCustomListComponent implements OnInit {
 
+  @Input() color: string;
   list = ["item 1", "item 2", "item 3"];
 
   constructor() { }
